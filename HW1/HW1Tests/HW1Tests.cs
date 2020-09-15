1b7c2513e6b34d2a0fd2d25440c7b4f907ecf39f
= <=>
2020-09-14 17:24:59 -0700

    Added base framework for project

---
+++ HW1/HW1Tests/HW1Tests.cs
@@ -0,0 +1,16 @@
+﻿// <copyright file="HW1Tests.cs" company="Connor Easton (11557902)">
+// Copyright (c) Connor Easton (11557902). All rights reserved.
+// </copyright>
+
+using System;
+using NUnit.Framework;
+
+namespace HW1.Tests
+{
+    public class HW1Tests
+    {
+        public void AllTests()
+        {
+        }
+    }
+}
