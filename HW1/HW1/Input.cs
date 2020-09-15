9290b229b946613500791f6736ca4a486775d3f3
Connor Easton <connor.easton@wsu.edu>
2020-09-14 23:32:02 -0700

    Added Input function and verrfied tests

---
+++ HW1/HW1/Input.cs
@@ -0,0 +1,26 @@
+﻿// <copyright file="Input.cs" company="Connor Easton (11557902)">
+// Copyright (c) Connor Easton (11557902). All rights reserved.
+// </copyright>
+
+using System;
+using System.IO;
+
+namespace HW1
+{
+    /// <summary>
+    /// Class for taking input in the project.
+    /// </summary>
+    public class Input
+    {
+        /// <summary>
+        /// Takes input from a TextReader.
+        /// </summary>
+        /// <param name="reader">TextReader variable.</param>
+        /// <returns>an array of parsed integers.</returns>
+        public int[] GetInput(TextReader reader)
+        {
+            int[] parsed = Array.ConvertAll(reader.ReadLine().Split(" "), int.Parse);
+            return parsed;
+        }
+    }
+}
--- HW1/HW1Tests/HW1Tests.cs
+++ HW1/HW1Tests/HW1Tests.cs
@@ -2,6 +2,7 @@
 // Copyright (c) Connor Easton (11557902). All rights reserved.
 // </copyright>
 
+using System.IO;
 using NUnit.Framework;
 
 namespace HW1.Test
@@ -9,17 +10,23 @@ namespace HW1.Test
     /// <summary>
     /// HW1 test class.
     /// </summary>
+    [TestFixture]
     public class HW1Tests
     {
         /// <summary>
         /// funciton to test inputs.
         /// </summary>
+        [Test]
         public void InputTests()
         {
             Input io = new Input();
             int[] inputList = { 1, 2, 3 };
 
-            Assert.AreEqual(inputList, io.GetInput());
+            const string inputString = "1 2 3";
+            var stringReader = new StringReader(inputString);
+
+            // normal use would use Console.in, for tests we make a string reader
+            Assert.AreEqual(inputList, io.GetInput(stringReader));
         }
     }
 }
