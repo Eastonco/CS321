ec21c0870e4e8c9382383567caf74f154fb832df
Connor Easton <connor.easton@wsu.edu>
2020-09-15 13:20:59 -0700

    Added BST tests and function framework

--- HW1/HW1/DataStructure/BST.cs
+++ HW1/HW1/DataStructure/BST.cs
@@ -1,5 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿// <copyright file="BST.cs" company="Connor Easton (11557902)">
+// Copyright (c) Connor Easton (11557902). All rights reserved.
+// </copyright>
+
 using System.Text;
 
 namespace BinarySearchTree
@@ -9,13 +11,78 @@ namespace BinarySearchTree
     /// </summary>
     public class BST
     {
-        private Node Root { get; set; }
-
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BST"/> class.
+        /// </summary>
         public BST()
         {
-
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BST"/> class.
+        /// </summary>
+        /// <param name="newroot">The Node of the root.</param>
+        public BST(Node newroot)
+        {
+        }
 
+        /// <summary>
+        /// Gets or sets root node of the BST.
+        /// </summary>
+        private Node Root { get; set; }
+
+        /// <summary>
+        /// Function to get the number of nodes in the BST.
+        /// </summary>
+        /// <returns>the number of nodes in the BST.</returns>
+        public int GetNodeCount()
+        {
+            return 0;
+        }
+
+        /// <summary>
+        /// Function to get the string of printed values of the tree.
+        /// </summary>
+        /// <returns>a string of the tree contents.</returns>
+        public string TraverseInOrder()
+        {
+            return null;
+        }
+
+        /// <summary>
+        /// Function to get level count.
+        /// </summary>
+        /// <returns>The number of levels in the tree.</returns>
+        public int GetLevelCount()
+        {
+            return 0;
+        }
+
+        /// <summary>
+        /// Function to get the minimum count of levels the tree could have.
+        /// </summary>
+        /// <returns>the minimum number of levels the BST could have.</returns>
+        public int GetMinLevelCount()
+        {
+            return 0;
+        }
+
+        /// <summary>
+        /// Insert a node into the tree.
+        /// </summary>
+        /// <param name="newnode">The node to be inserted.</param>
+        public void Insert(Node newnode)
+        {
+        }
+
+        /// <summary>
+        /// recursive function to help build the in order traversal string.
+        /// </summary>
+        /// <param name="output">a string to be built on.</param>
+        /// <returns>the built string.</returns>
+        private string TraversalHelper(StringBuilder output)
+        {
+            return null;
+        }
     }
 }
--- HW1/HW1Tests/BSTTests.cs
+++ HW1/HW1Tests/BSTTests.cs
@@ -13,7 +13,7 @@ namespace BinarySearchTree
     public class BSTTests
     {
         /// <summary>
-        /// Tests for the Binary Search Tree.
+        /// Tests node creation.
         /// </summary>
         [Test]
         public void NodeCreation()
@@ -23,6 +23,104 @@ namespace BinarySearchTree
 
             newNode.Data = 1;
             Assert.AreEqual(newNode.Data, 1);
+        }
+
+        /// <summary>
+        /// Empty tree creation.
+        /// </summary>
+        [Test]
+        public void EmptyBSTCreation()
+        {
+            BST tree = new BST();
+            Assert.AreEqual(tree.TraverseInOrder(), string.Empty);
+        }
+
+        /// <summary>
+        /// tree creation with a root node.
+        /// </summary>
+        [Test]
+        public void BSTCreationWithNode()
+        {
+            BST tree = new BST(new Node(5));
+            Assert.AreEqual(tree.TraverseInOrder(), "5");
+        }
+
+        /// <summary>
+        /// Inserts a node to the BST.
+        /// </summary>
+        [Test]
+        public void InsertToBST()
+        {
+            BST tree = new BST();
+            tree.Insert(new Node(10));
+            Assert.AreEqual(tree.TraverseInOrder(), "10");
+        }
+
+        /// <summary>
+        /// Inserts multiple elements out of order and tests the sorting.
+        /// </summary>
+        [Test]
+        public void SortBST()
+        {
+            BST tree = new BST();
+            tree.Insert(new Node(10));
+            tree.Insert(new Node(8));
+            tree.Insert(new Node(6));
+            tree.Insert(new Node(4));
+            tree.Insert(new Node(2));
+            tree.Insert(new Node(0));
+
+            Assert.AreEqual(tree.TraverseInOrder(), "0 2 4 6 8 10");
+        }
+
+        /// <summary>
+        /// Inserts multiple elements out of order and tests node count.
+        /// </summary>
+        [Test]
+        public void BSTNodeCount()
+        {
+            BST tree = new BST();
+            tree.Insert(new Node(10));
+            tree.Insert(new Node(8));
+            tree.Insert(new Node(6));
+            tree.Insert(new Node(4));
+            tree.Insert(new Node(2));
+            tree.Insert(new Node(0));
+
+            Assert.AreEqual(tree.GetNodeCount(), 5);
+        }
+
+        /// <summary>
+        /// Inserts multiple elements out of order and tests node count.
+        /// </summary>
+        [Test]
+        public void BSTMinLevelCount()
+        {
+            BST tree = new BST();
+            tree.Insert(new Node(10));
+            tree.Insert(new Node(8));
+            tree.Insert(new Node(6));
+            tree.Insert(new Node(4));
+            tree.Insert(new Node(2));
+            tree.Insert(new Node(0));
+
+            Assert.AreEqual(tree.GetMinLevelCount(), 3);
+        }
+
+        /// <summary>
+        /// Inserts multiple elements out of order and tests level count.
+        /// </summary>
+        [Test]
+        public void BSTLevelCount()
+        {
+            BST tree = new BST();
+            tree.Insert(new Node(5));
+            tree.Insert(new Node(6));
+            tree.Insert(new Node(4));
+            tree.Insert(new Node(2));
+            tree.Insert(new Node(1));
+
+            Assert.AreEqual(tree.GetLevelCount(), 4);
         }
     }
 }
