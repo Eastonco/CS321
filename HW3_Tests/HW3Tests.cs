83314e88b20f4fe0464bf24abfb72d23eabd85db
Connor Easton <connor.easton@wsu.edu>
2020-09-30 21:24:11 -0700

    added test framework

---
+++ HW3_Tests/HW3Tests.cs
@@ -0,0 +1,24 @@
+﻿// <copyright file="HW3Tests.cs" company="Connor Easton (11557902)">
+// Copyright (c) Connor Easton (11557902). All rights reserved.
+// </copyright>
+
+using System;
+using NUnit.Framework;
+
+namespace HW3_Tests
+{
+    /// <summary>
+    /// Tests for HW3.
+    /// </summary>
+    [TestFixture]
+    public class HW3Tests
+    {
+        /// <summary>
+        /// A sample test.
+        /// </summary>
+        [Test]
+        public void SampleTest()
+        {
+        }
+    }
+}
